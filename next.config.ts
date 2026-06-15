e28b76f6cf4c5af8410f5ec4249452732ae8c684
Mayank Shukal <25it094@charusat.edu.in>
2026-06-15 21:03:35 +0530

    Revert change

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Add this: makes the "next build" command work in deployment
-  output: "export",
-
-  // Add this to fix the "404" on subpages like /services
-  trailingSlash: true,
+  /* config options here */
 };
 
 export default nextConfig;
